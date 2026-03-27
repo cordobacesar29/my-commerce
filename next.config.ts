c809bb68918e2bfdb0a1ee68068bf8f1c229ba3a
Cesar Cordoba <cordobacesar29@gmail.com>
2026-03-27 16:27:44 -0300

    add image config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  images: {
+    domains: ['api.together.xyz', 'firebasestorage.googleapis.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // Dominio de fotos de Google
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
