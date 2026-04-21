5161587568991a1b4b367361c9e6aa0eda5ec25d
Cesar Cordoba <cordobacesar29@gmail.com>
2026-04-21 14:12:04 -0300

    users and roles types

---
+++ src/types/roles.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const UserRoleSchema = z.enum([
+  "SUPER_ADMIN",
+  "STORE_OWNER",
+  "CUSTOMER",
+]);
+
+export type IUserRole = z.infer<typeof UserRoleSchema>;
+
+export const USER_ROLES = {
+  SUPER_ADMIN: "SUPER_ADMIN",
+  STORE_OWNER: "STORE_OWNER",
+  CUSTOMER: "CUSTOMER",
+} as const satisfies Record<IUserRole, IUserRole>;
